ccb38f3668ac4a76bc99d3b0dac24991ad5cf295
Ponkin <alexey.ponkin@yandex.ru>
2014-05-05 20:19:09 +0400

    added date field
    added date field to inbox message

--- js/filters.js
+++ js/filters.js
@@ -7,4 +7,10 @@ angular.module('junkMailApp.filters', []).
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     };
-  }]);
+  }])
+  .filter('toLocalTime', function(){
+	  return function(time) {
+	      return new Date(time).toLocaleString();
+	    };
+  })
+  ;
