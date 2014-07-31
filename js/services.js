9c6ecdcb9e09f5d1eaf9f0d5ab244a895e9435c5
Ponkin <alexey.ponkin@yandex.ru>
2014-08-01 01:45:59 +0400

    refactoring

--- js/services.js
+++ js/services.js
@@ -6,41 +6,53 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('junkMailApp.services', ['firebase'])
-  .value('version', '0.1')
+	.value('version', '0.1')
+	.value('base_url', 'https://sweltering-fire-2054.firebaseio.com/')
+	.factory('InboxService', ['$q', '$firebase', 'base_url',
+		function($q, $firebase, base_url) {
+			var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+			var text = '';
 
-  .factory('FirebaseService', [ '$firebase', function($firebase) {
-    var inboxesRef = new Firebase('https://sweltering-fire-2054.firebaseio.com/inboxes');
-    var newInboxRef = inboxesRef.push();
-  
-    newInboxRef.onDisconnect().remove();
-    newInboxRef.set({
-  	  meta: Math.round(Math.random()*101)
-    });
-  
-    var messagesRef = newInboxRef.child('messages');
-	
-	var now = new Date();
-	
-  
-    messagesRef.set({
-  	  '0' :{
-  	  		from: 'admin@flashmail.com',
-			subject: 'About service usage',
-			date: now.getTime(),
-  	  		body: 'Hello World'
-  		}
-    });  
-	
-	return {
-		getFirebase: function(){
-			return $firebase(messagesRef);
-		},
-		getInboxURL: function(){
-			return newInboxRef.toString();
+			var baseRef = new Firebase(base_url);
+			var messagesRef = $q.defer();
+
+			for (var i = 0; i < 5; i++)
+				text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+			var auth = new FirebaseSimpleLogin(baseRef, function(error, user) {
+				if (error) {
+					// an error occurred while attempting login
+					console.log(error);
+					messagesRef.reject(error);
+				} else if (user) {
+					// user authenticated with Firebase
+					console.log("User ID: " + user.uid + ", Provider: " + user.provider);
+					var userDir = baseRef.child('users').child(text);
+					userDir.set({
+						user_id: user.uid,
+						messages: {
+							0: {
+								"body": "Hello World",
+								"date": new Date().getTime(),
+								"from": "admin@flashmail.com",
+								"subject": "About service usage"
+							}
+						}
+					});
+					messagesRef.resolve($firebase(userDir.child('messages')));
+				} else {
+					// user is logged out
+					console.log("User logout");
+				}
+			});
+
+			return {
+				getInboxName: function() {
+					return text;
+				},
+				getMessages: function() {
+					return messagesRef.promise;
+				}
+			};
 		}
-	};
-  
-
-    } ]
-	
-);
+	]);
